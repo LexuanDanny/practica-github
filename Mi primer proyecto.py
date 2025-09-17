9fa40ac6cf64a5581c47d080f4c93e3d0897146c
LexuanDanny <lexuan.peng@campus.xaloc.org>
2025-09-17 09:31:10 +0200

    Proyecto Python
    Mi primer ejercicio Python

---
+++ Mi primer proyecto.py
@@ -0,0 +1,33 @@
+var1="Xaloc"
+var2=4
+var3=5
+var4=1.8
+
+print("Hola mi nombre es Lexuan")
+
+suma=var2+var3
+print(suma)
+print(var2+var3)
+print("El resultado de la suma de var2+var3 es",suma)
+print("Error de instrucción")
+
+nombre=input("Introduce tu nombre")
+apellido1=input("Introduce tu primer apellido")
+apellido2=input("Introduce tu segundo apellido")
+
+nombre_completo=nombre+""+apellido1+""+apellido2
+
+print(nombre_completo)
+suma=var2+var3
+print(f"La suma de {var2}+{var3} es igual a",suma)
+resta=var2-var3
+print(f"La resta de {var2}-{var3} es igual a",resta)
+multiplicacion=var2*var3
+print(f"La multiplicación de {var2}*{var3} es igual a",multiplicacion)
+divicion=var2/var3
+print(f"La divición de {var2}/{var3} es igual a",divicion)
+division_entera=var2//var3
+print(f"La división entera de {var2}/{var3} es igual a",division_entera)
+modulo_division=var2%var3
+print(f"El resto de la división de {var2}+{var3} es igual a",modulo_division)
+
